8c3535cd64ddf6e0aeaa84af96a331e08591def9
andreimoreira <dedeigalinha@hotmail.com>
2025-09-29 09:43:11 -0300

    melhorias do exercicio de POO feitas

--- POO.php
+++ POO.php
@@ -2,25 +2,33 @@
 
 class ItensBiblioteca
 {
-    protected $titulo;
-    private $disponivel;
+    protected string $titulo;
+    private bool $disponivel;
+    protected DateTime $dataEmprestimo;
+    protected int $contadorEmprestimo;
 
     public function __construct($titulo)
     {
         $this->disponivel = true;
         $this->titulo = $titulo;
     }
-    public function emprestar()
+    public function emprestar() : void
     {
         if ($this->disponivel === true) {
             $this->disponivel = false;
+            $this->contadorEmprestimo++;
+            $this->dataEmprestimo = new DateTime();
             echo "O livro foi emprestado";
         } else {
             echo "o livro ja foi emprestado";
         }
+
     }
 
-    public function devolver()
+    public function getTitulo() : string {
+        return $this->titulo;
+    }
+    public function devolver() : void
     {
         if ($this->disponivel === false) {
             $this->disponivel = true;
@@ -30,8 +38,12 @@ class ItensBiblioteca
         }
     }
 
-    public function getDisponivel() {
+    public function getDisponivel()  : bool {
         return $this->disponivel;
+    }
+
+    public function getContadorEmprestimo() : int {
+        return $this->contadorEmprestimo;
     }
 }
 
@@ -58,20 +70,48 @@ class Revista extends ItensBiblioteca
     protected $autor;
     protected $ano;
 
-    public function emprestar()
+    public function emprestar() : void
     {
         echo "A revista foi emprestada!";
     }
 
-    public function devolver()
+    public function devolver() : void
     {
         echo "A revista foi devolvida!";
     }
 }
 
-$objeto = new Livro("Harry Potter", "João", 2014);
-echo $objeto->exibirInfo();
+class Usuario {
+    protected string $usuario;
+    protected string $senha;
+    private int $id;
+    protected array $historico = [];
 
-$objeto->emprestar();
+    public function __construct($usuario, $senha){
+        $this->usuario = $usuario;
+        $this->id = rand(1, 1000);
+        $this->senha = $senha;
+    }
 
-$objeto->devolver();
+    public function autenticar(string $senha) {
+        if($this->senha === $senha) {
+            return "acesso permitido";
+        } else {
+            return "senha errada";
+        }
+    }
+
+    public function registrarEmprestimo(ItensBiblioteca $item) {
+          $this->historico[] = $item->getTitulo();
+    }
+    public function getHistorico() : array {
+        return $this->historico;
+    }
+
+}
+class Biblioteca {
+
+}
+
+$usuario = new Usuario("dedei", "123abc");
+
