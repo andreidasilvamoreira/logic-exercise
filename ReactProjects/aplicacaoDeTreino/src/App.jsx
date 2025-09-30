99565dc9879511de47a40987b8bd1768d6b06668
andreimoreira <dedeigalinha@hotmail.com>
2025-09-30 15:34:42 -0300

    small adjustments

--- ReactProjects/aplicacaoDeTreino/src/App.jsx
+++ ReactProjects/aplicacaoDeTreino/src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   return (
     <>
       <div>
-        <Contador valorInicial={10} quantidade={3}/>
+        <Contador valorInicial={10} quantidade={5} min={0} max={1000} />
+        <Contador valorInicial={20} quantidade={5} min={0} max={200} />
+        <Contador valorInicial={100} quantidade={5} min={0} max={1100} />
+        <Contador valorInicial={1} quantidade={5} min={0} max={10}/>
       </div>
     </>
   )
--- ReactProjects/aplicacaoDeTreino/src/components/contador.jsx
+++ ReactProjects/aplicacaoDeTreino/src/components/contador.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import Botao from "./botao";
 
-function Contador({ valorInicial, quantidade = 3 }) {
+function Contador({ valorInicial, quantidade = 3, min, max }) {
     const [contador, setContador] = useState(valorInicial)
 
+    function incrementar() {
+        if(contador < max) {
+            setContador(contador + quantidade)
+        }
+    }
+    function decrementar() {
+        if(contador > min) {
+            setContador(contador - quantidade)
+        }
+    }
     return (
         <div>
-            <Botao text="-" quantidade={quantidade} onClick={() => setContador(contador - quantidade)}/>
-            <Botao text="+" quantidade={quantidade} onClick={() => setContador(contador + quantidade)}/>
+            <Botao text="-" quantidade={quantidade} onClick={decrementar}/>
+            <Botao text="+" quantidade={quantidade} onClick={incrementar}/>
             <h1>{contador}</h1>
             <Botao text="resetar" onClick={() => setContador(valorInicial)}/>
         </div>
